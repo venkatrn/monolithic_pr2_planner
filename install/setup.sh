81d7ecffaac3621838ff9e31d1106a42b7f9feb6
Shivam Vats <shivamvats.iitkgp@gmail.com>
2016-06-03 18:40:03 +0100

    setup.sh should run rosdep install

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp .rosinstall ../../;
+echo "Installing dependencies from github"
+wstool init ../.. .rosinstall;
 cd ../../;
 wstool update;
+
+echo "Installing debian packages using apt-get"
+rosdep install --from-paths . --ignore-src --rosdistro=groovy
 
